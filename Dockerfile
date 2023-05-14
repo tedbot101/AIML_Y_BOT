d8c33f6d7544c333a1ebd3df96d8766a614f4ddf
Ted Liu <tedliu1999@gmail.com>
2023-05-14 23:49:33 +0630

    update

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,25 @@
 FROM ubuntu:18.04
 
-RUN sudo apt-get update
-RUN sudo apt-get install -y python3.7.4 && apt-get install -y git
+RUN apt-get update
+RUN apt-get install -y python3 && apt-get install -y python3-pip
 
-RUN pyhon -m pip install Flask==2.1.0
-RUN pyhon -mpip install --no-cache-dir programy
+RUN python3 -m pip install Flask==2.0.3
+RUN python3 -m pip install --no-cache-dir programy
 
-RUN mkdir chatbot
-RUN cd chatbot
-RUN git clone 
 
+RUN mkdir /chatbot
+ADD . /chatbot
+WORKDIR /chatbot
+
+
+
+# RUN mkdir chatbot
+# RUN cd chatbot
+# RUN git clone https://github.com/tedbot101/AIML_Y_BOT.git
+
+# RUN python custom_bot.py
+
+# VOLUME /gluon
+# WORKDIR /gluon
+# RUN git checkout docker
+# RUN cp -a docs/site-example/ site
---
+++ custom_bot.py
@@ -0,0 +1,61 @@
+from programy.clients.embed.configfile import EmbeddedConfigFileBot
+import test_data
+import time
+
+# initialization
+config_file = "./config/windows/config.yaml"
+logging_file = "./config/windows/logging.yaml"
+question = test_data.questions
+
+# bot
+bot = EmbeddedConfigFileBot(config_file, logging_file)
+client_context = bot.create_client_context("test_user")
+
+
+def chatbot_testing(questions):
+    total_question = len(questions)
+    true_counter = 0
+    for question in questions:
+        # question[0] is the test string, [1] is the expected intent
+        to_test = bot.process_question(client_context, question[0])
+        print("[test] Question :" + question[0])
+        print("[test] Chatbot Reponse :" + to_test)
+        print()
+
+        result = input('[bot] Is the output correct (Y/N) : ')
+        if result == 'y':
+            true_counter += 1
+            result =''
+        else:
+            true_counter += 0
+            print('no')
+        # if to_test[1] == question[1]: 
+        #     
+    
+    print('[test] Tests Count : ', total_question)
+    print('[test] Accuracy : '+ str(true_counter) +'/' + str(total_question) + ", (" + str(( true_counter / total_question ) * 100) + "%)")
+
+
+
+
+
+
+### bot interface ###
+print("[bot] Napier AIML Tedbot")
+
+
+while True:
+    start = time.time()
+    user_input = input(">>")
+    end = time.time()
+    print("Response time :", round(end - start) , 'ms')
+    if user_input == "exit" :
+        print("[bot] Shutting down bot. Bye Bye ~")
+        break
+    
+    elif user_input == "testing" :
+        chatbot_testing(test_data.questions)
+
+    else:
+        print(bot.process_question(client_context,user_input))
+        # print("[bot]" +  bot.process_question(client_context,user_input))
